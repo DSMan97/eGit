c5630e7f3fa0d38ef1647bc47738dc771817cc55
DSMan97 <guillecrespoaguayo@gmail.com>
2017-12-17 21:58:50 +0100

    Prueba

---
+++ Tema3/src/Arrays/Ejercicio2.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String [] diez;
+		diez = new String [10];
+		
+		//O  Declarar
+		
+		String [] diez1 = new String [2];
+		diez1[0]="YES";
+		diez1[1]="NO";
+		String[] prueba= {"Si", "No"};
+		
+	}
+
+}
