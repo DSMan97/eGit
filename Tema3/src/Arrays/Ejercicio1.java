02f5454ac411b57b62c6b9ec3492409949061dd2
DSMan97 <guillecrespoaguayo@gmail.com>
2017-12-17 21:56:34 +0100

    Primer Commit

---
+++ Tema3/src/Arrays/Ejercicio1.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] primos;
+		primos=new int[5];
+		primos[0]=2;
+		primos[1]=3;
+		primos[2]=5;
+		primos[3]=7;
+		primos[4]=11;
+		System.out.println(Arrays.toString(primos));
+	}
+}
